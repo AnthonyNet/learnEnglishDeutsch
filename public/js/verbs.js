7df831df20dbad6cc5dddb2aab25cd8eba38db53
AnthonyZet <67970914+AnthonyZet@users.noreply.github.com>
2021-09-18 14:48:56 +0200

    verbs.js updated, input function updated by font color change

--- public/js/verbs.js
+++ public/js/verbs.js
@@ -1,15 +1,40 @@
 
 //Pro všechny inputy označ zeleně správnou odpověď a červeně špatnou
 
-let all = document.querySelectorAll('#slovo_ID')
-all.forEach(element => element.addEventListener('input',(e)=>{
-    if(e.target.value === element.getAttribute('data-verb')){
-    element.style.border = "5px solid green"
-    //slovo.classList.add('tvoj-class')
-    }else{element.style.border = "5px solid red"};
-    }));
 
-   
+
+let check = document.querySelectorAll('.input_Text')
+check.forEach(element => element.addEventListener('input',(e)=>{
+   var data = element.getAttribute('data-verb').toString();
+   var value = e.target.value;
+   if (e.target.value == ""){
+    element.style.border = "none";
+    element.style.color = "black";
+   }
+   else if (data.startsWith(value)){
+      element.style.border = "5px solid green";
+      element.style.color = "green";
+   }
+   else{
+      element.style.border = "5px solid red";
+      element.style.color = "red";
+   }
+}));
+
+
+
+
+/*
+    let allX = document.querySelectorAll('.input_Text')
+    allX.forEach(element => element.addEventListener('input',(e)=>{
+        if(e.target.value === element.getAttribute('data-verb')){
+        element.style.border = "5px solid green"
+        
+        }else{element.style.border = "5px solid red"};
+        }));*/
+        
+
+
 
     (function($) {
 
